adb5b77852db00525065b1e86ff0e298f1d0f7c5
gRizzlyGR <giusrizzi@gmail.com>
2019-07-19 15:40:05 +0200

    Add day 6 solutions

---
+++ day-06/part-02/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type coords struct {
+	row int
+	col int
+}
+
+var arr = [1000][1000]int{}
+var grid = arr[:]
+
+func main() {
+	file, err := os.Open("../input")
+	if err != nil {
+		log.Fatalln("Cannot read file", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		light(s)
+	}
+
+	fmt.Println(howMuchIsBright())
+
+}
+
+func light(s string) {
+	command := strings.Split(s, " ")
+
+	switch command[0] {
+	case "toggle":
+		toggle(getCoords(command[1]), getCoords(command[3]))
+	case "turn":
+		switch command[1] {
+		case "on":
+			turnOn(getCoords(command[2]), getCoords(command[4]))
+		case "off":
+			turnOff(getCoords(command[2]), getCoords(command[4]))
+		}
+	}
+}
+
+func getCoords(pair string) coords {
+	a := strings.Split(pair, ",")
+	row, _ := strconv.Atoi(a[0])
+	col, _ := strconv.Atoi(a[1])
+	return coords{row, col}
+}
+
+func toggle(from, to coords) {
+	for i := from.row; i <= to.row; i++ {
+		for j := from.col; j <= to.col; j++ {
+			grid[i][j] = grid[i][j] + 2
+		}
+	}
+}
+
+func turnOn(from, to coords) {
+	for i := from.row; i <= to.row; i++ {
+		for j := from.col; j <= to.col; j++ {
+			grid[i][j]++
+		}
+	}
+}
+
+func turnOff(from, to coords) {
+	for i := from.row; i <= to.row; i++ {
+		for j := from.col; j <= to.col; j++ {
+			tmp := grid[i][j]
+			tmp--
+			if tmp < 0 {
+				tmp = 0
+			}
+			grid[i][j] = tmp
+		}
+	}
+}
+
+func howMuchIsBright() int {
+	sum := 0
+	for _, slice := range grid {
+		for _, light := range slice {
+			sum += light
+		}
+	}
+
+	return sum
+}
