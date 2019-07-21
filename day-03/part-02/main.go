8b831ec19581bf96d35ef38a73b7293f2ce736c5
gRizzlyGR <giusrizzi@gmail.com>
2019-07-21 20:59:13 +0200

    Add day 3 part 2 solution

---
+++ day-03/part-02/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+var n = byte('^')
+var s = byte('v')
+var e = byte('>')
+var w = byte('<')
+
+type coords struct {
+	row int
+	col int
+}
+
+func main() {
+	buffer, err := ioutil.ReadFile("../input")
+	if err != nil {
+		log.Fatalln("Cannot read file", err)
+	}
+
+	fmt.Println(move(buffer))
+
+}
+
+func move(path []byte) int {
+	grid := make(map[coords]int)
+
+	origin := coords{0, 0}
+	grid[origin] += 2 // The two Santas start delivering at the same house
+
+	c := make(chan coords)
+	go deliver(path, 0, 2, origin, c) // Santa
+	go deliver(path, 1, 2, origin, c) // Robo-Santa
+
+	updateMap(grid, c, len(path))
+
+	close(c)
+
+	return len(grid)
+}
+
+func deliver(path []byte, start int, step int, point coords, c chan<- coords) {
+	for i := start; i < len(path); i += step {
+		point = next(point, path[i])
+		c <- point
+	}
+}
+
+func updateMap(grid map[coords]int, c <-chan coords, limit int) {
+	for i := 0; i < limit; i++ {
+		grid[<-c]++
+	}
+}
+
+func next(origin coords, direction byte) coords {
+	switch direction {
+	case n:
+		return coords{origin.row - 1, origin.col}
+	case s:
+		return coords{origin.row + 1, origin.col}
+	case e:
+		return coords{origin.row, origin.col + 1}
+	case w:
+		return coords{origin.row, origin.col - 1}
+	default:
+		log.Fatalln("Unknown direction:", string(direction))
+	}
+
+	return coords{0, 0}
+}
